866baf795e252768b56dadb5113cc2f9086cfc02
o39nq1 <andras.hortobagyi@stud.uni-corvinus.hu>
2023-11-14 11:03:18 +0100

    uj

--- 9gyak/Controllers/JokeController.cs
+++ 9gyak/Controllers/JokeController.cs
@@ -1,43 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _9gyak.JokeModels;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace _9gyak.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/index")]
     [ApiController]
     public class JokeController : ControllerBase
     {
-        // GET: api/<JokeController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            return Ok(context.Jokes.ToList());
         }
-
-        // GET api/<JokeController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var keresettVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            if (keresettVicc == null)
+            {
+                return NotFound($"Nincs #{id} azonosítóval vicc");
+            }
+            else
+            {
+                return Ok(keresettVicc);
+            }
         }
 
-        // POST api/<JokeController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Joke újVicc)
         {
-        }
-
-        // PUT api/<JokeController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<JokeController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(újVicc);
+            context.SaveChanges();
         }
     }
 }
